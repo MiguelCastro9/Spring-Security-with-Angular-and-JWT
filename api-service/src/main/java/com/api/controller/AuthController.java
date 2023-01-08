660d9fd303ae751426cdc26bfa0cda82c5283a1e
miguel castro <miguel.castro9@hotmail.com>
2023-01-08 14:00:25 -0300

    add autenticacao no controller

---
+++ api-service/src/main/java/com/api/controller/AuthController.java
@@ -0,0 +1,49 @@
+package com.api.controller;
+
+import com.api.dto.JwtDto;
+import com.api.jwt.JwtProvider;
+import com.api.model.UsuarioModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Miguel Castro
+ */
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtDto> login(@RequestBody UsuarioModel usuarioModel) {
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(usuarioModel.getEmail(), usuarioModel.getSenha()));
+
+        //A partir da autenticação gera o token.
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtProvider.gerarToken(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+
+        return new ResponseEntity<>(jwtDto, HttpStatus.OK);
+    }
+}
