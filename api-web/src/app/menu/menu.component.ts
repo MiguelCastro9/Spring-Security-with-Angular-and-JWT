bf37f08b46c6f8c46db4f0c8bb11e4bf8a6da613
miguel castro <miguel.castro9@hotmail.com>
2023-01-08 15:11:23 -0300

    componente menu e usuario

--- api-web/src/app/menu/menu.component.ts
+++ api-web/src/app/menu/menu.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  logout() {
+
+    window.sessionStorage.clear();
+    window.location.reload();
+    this.router.navigate(['login']);
+  }
 
 }
--- api-web/src/app/usuario/usuario.component.ts
+++ api-web/src/app/usuario/usuario.component.ts
@@ -1,10 +1,132 @@
-import { Component } from '@angular/core';
+import { TokenService } from './../service/token.service';
+import { UsuarioService } from './../service/usuario.service';
+import { UsuarioDto } from './../model/usuarioDto';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.css']
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit {
 
+  usuarioDto = new UsuarioDto();
+  usuarioLista: UsuarioDto[] = [];
+  verifica = false;
+  roles!: string[];
+  isAdmin = false;
+  isGestor = false;
+  isCriador = false;
+  isEditor = false;
+  senhaRepetida: string = '';
+  successMensagem = '';
+  alertMensagem = '';
+
+  constructor(private usuarioService: UsuarioService, private tokenService: TokenService) { }
+
+  ngOnInit(): void {
+
+    if (this.tokenService.getToken()) {
+
+      this.roles = this.tokenService.getAuthorities();
+      this.roles.forEach(dados => {
+        if (dados == 'ROLE_ADMIN') {
+          this.isAdmin = true;
+        }
+        if (dados == 'ROLE_GESTOR') {
+          this.isGestor = true;
+        }
+        if (dados == 'ROLE_CRIADOR') {
+          this.isCriador = true;
+        }
+        if (dados == 'ROLE_EDITOR') {
+          this.isEditor = true;
+        }
+      })
+    } else {
+
+      this.isGestor = false;
+      this.isAdmin = false;
+    }
+
+    this.listar();
+
+  }
+
+  listar() {
+
+    this.usuarioService.listar().subscribe(dados => this.usuarioLista = dados);
+
+  }
+
+  validarFormulario() {
+
+    if (!this.usuarioDto.nome) {
+      this.alertMensagem = 'Campo nome é obrigatório.';
+      return false;
+    }
+    if (!this.usuarioDto.email) {
+      this.alertMensagem = 'Campo e-mail é obrigatório.';
+      return false;
+    }
+    if (!this.usuarioDto.senha) {
+      this.alertMensagem = 'Campo senha é obrigatório.';
+      return false;
+    }
+    if (!this.senhaRepetida) {
+      this.alertMensagem = 'Campo repetir senha é obrigatório.';
+      return false;
+    }
+    if (this.usuarioDto.senha != this.senhaRepetida) {
+      this.alertMensagem = 'As senhas não são iguais.';
+      return false;
+    }
+    if (!this.usuarioDto.roleValor) {
+      this.alertMensagem = 'Campo permissão é obrigatório.';
+      return false;
+    }
+    if (!this.usuarioDto.status) {
+      this.alertMensagem = 'Campo status é obrigatório.';
+      return false;
+    }
+
+    return true;
+  }
+
+  salvar(usuarioDto: UsuarioDto) {
+
+    if (!this.validarFormulario()) {
+      return;
+    }
+
+    if (!this.verifica) {
+      this.usuarioService.novo(usuarioDto).subscribe().unsubscribe();
+      alert('Usuário salvo com sucesso!')
+      window.location.reload();
+
+    } else {
+      this.usuarioService.editar(usuarioDto).subscribe().unsubscribe();
+      alert('Usuário editado com sucesso!')
+      window.location.reload();
+  }
+}
+
+  editar(usuarioDto: UsuarioDto) {
+
+    this.verifica = true;
+    localStorage.setItem('id', usuarioDto.id.toString());
+    let id = localStorage.getItem('id');
+    this.usuarioService.buscar(id)
+    .subscribe(dados => this.usuarioDto = dados);
+  }
+
+  deletar(usuarioDto: UsuarioDto) {
+
+    if(confirm("Deseja deletar este usuário ?")){
+      this.usuarioService.deletar(usuarioDto)
+      .subscribe(dados => this.usuarioLista = this.usuarioLista.filter(p => usuarioDto));
+      alert('Usuário deletado com sucesso!');
+      window.location.reload();
+    }
+  }
 }
