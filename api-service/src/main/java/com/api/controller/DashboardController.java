40d564e7b8774f962206d33bf5a8574a7ed126cb
miguel castro <miguel.castro9@hotmail.com>
2023-01-08 15:29:09 -0300

    controller dashboard

---
+++ api-service/src/main/java/com/api/controller/DashboardController.java
@@ -0,0 +1,41 @@
+package com.api.controller;
+
+import com.api.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Miguel Castro
+ */
+@RestController
+@RequestMapping("/dashboard")
+@CrossOrigin(origins = "*")
+public class DashboardController {
+    
+    @Autowired
+    UsuarioService usuarioService;
+    
+    @GetMapping("/ativo")
+    public ResponseEntity<Integer> countAtivo() {
+        
+        return new ResponseEntity<>(usuarioService.countUsuarioActive(), HttpStatus.OK);
+    }
+    
+    @GetMapping("/inativo")
+    public ResponseEntity<Integer> countInativo() {
+        
+        return new ResponseEntity<>(usuarioService.countUsuarioInactive(), HttpStatus.OK);
+    }
+    
+    @GetMapping("/admin")
+    public ResponseEntity<Integer> countAdmin() {
+        
+        return new ResponseEntity<>(usuarioService.countUsuarioAdmin(), HttpStatus.OK);
+    }
+}
--- api-service/src/main/java/com/api/repository/UsuarioRepository.java
+++ api-service/src/main/java/com/api/repository/UsuarioRepository.java
@@ -2,11 +2,8 @@ package com.api.repository;
 
 import com.api.model.UsuarioModel;
 import java.util.Optional;
-import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
